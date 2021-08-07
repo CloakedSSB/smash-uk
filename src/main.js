b1f1a6360c609232aa584522828cabe9ca7e91a9
Jack Settle <jackdmike.settle@gmail.com>
2021-08-07 16:21:26 +0100

    [SBUK] added firebase

--- src/main.js
+++ src/main.js
@@ -1,10 +1,26 @@
 import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
+import vuetify from './plugins/vuetify'
+import { initializeApp } from 'firebase/app'
 
 Vue.config.productionTip = false
 
+const firebaseConfig = {
+  apiKey: "AIzaSyBO_p7DSgloGKlw2Xxlde9QnqEkJideksY",
+  authDomain: "smash-bros-uk.firebaseapp.com",
+  databaseURL: "https://smash-bros-uk-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "smash-bros-uk",
+  storageBucket: "smash-bros-uk.appspot.com",
+  messagingSenderId: "634228887709",
+  appId: "1:634228887709:web:6c32ed2f5a62ac284fc474",
+  measurementId: "G-WZRTHF5LTJ"
+};
+
+const firebaseApp = initializeApp(firebaseConfig)
+
 new Vue({
   store,
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
