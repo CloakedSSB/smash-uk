7863539914e3f603b916d23ab406bb6cc2405e6f
Jack Settle <jackdmike.settle@gmail.com>
2021-08-08 18:59:18 +0100

    [SBUK-12] Added router

---
+++ src/views/home.vue
@@ -0,0 +1,13 @@
+<template>
+    <v-card class="my-5 mx-5">
+        <v-card-title>Region Information</v-card-title>
+        <region-select></region-select>
+    </v-card>
+</template>
+<script>
+import RegionSelect from '../components/RegionSelect.vue'
+export default {
+    components: { RegionSelect },
+    name: "home"
+}
+</script>
---
+++ src/views/region.vue
@@ -0,0 +1,8 @@
+<template>
+    <h1>{{ $route.params.regionName }}</h1>
+</template>
+<script>
+export default {
+    name: "region"
+}
+</script>
