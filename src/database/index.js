3cb23cc7081c5a0e4b252333f2e033303a69dc79
Jack Settle <jackdmike.settle@gmail.com>
2021-08-08 19:00:28 +0100

    [SBUK-12] Added database stuuf (dummy data rn)

---
+++ src/database/index.js
@@ -0,0 +1,5 @@
+async function getRegionMap() {
+    return {"North": "n" , "East": "e", "South": "s", "West": "w"};
+}
+
+export { getRegionMap }
--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,24 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import _ from 'lodash'
+import { getRegionMap } from '../database'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    regionMap: {},
+    invertedRegionMap: {}
   },
   mutations: {
+    updateRegionMap (state, newMap) {
+      state.regionMap = Object.assign({}, newMap)
+      state.invertedRegionMap = Object.assign({}, _.invert(newMap))
+    } 
   },
   actions: {
-  },
-  modules: {
+    async getRegionMap( {commit} ) {
+      commit('updateRegionMap', await getRegionMap())
+    }
   }
 })
